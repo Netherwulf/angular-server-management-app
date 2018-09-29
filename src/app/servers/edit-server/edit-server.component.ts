dec720523fa8b1bb8caccf3011938534a4a027c8
Kamil Kanclerz <kanclerz3@tlen.pl>
2018-09-29 11:56:48 +0200

    Added passing query parameters to nested routes
    Added passing query parameters to nested routes

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,6 +12,7 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
+  allowEdit = false;
 
   constructor(private serversService: ServersService,
               private route: ActivatedRoute) { }
@@ -21,7 +22,14 @@ export class EditServerComponent implements OnInit {
     console.log(this.route.snapshot.fragment);
     // this.route.queryParams.subscribe();  // reactive approach
     // this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    this.route.queryParams
+      .subscribe(
+        (queryParams: Params) => {
+          this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
+        }
+      );
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
